3c2ea1b0a6d753afd5f004a947652e2940fefb0d
Kuba <jmn417@nyu.edu>
2018-01-28 18:46:20 -0500

    removed most globals

--- telecast.py
+++ telecast.py
@@ -10,42 +10,9 @@ import time
 browser = webdriver.Chrome()
 browser.get('https://web.telegram.org/#/im')
 #________________________________________________________________________#
-#Global variables. get rid of these fucking things ASAP
-
-# Home Group
-global HOME_GROUP
-HOME_GROUP = 'PAID GROUP'
-
-# GroupQueue
-global groupQueue
-groupQueue = deque([])
-
-#groupChats
-global groupChats
-
-#Selected Group
-global selectedGroup
-
-#Last Messagege Dictionary
-global lastMessageDict
-lastMessageDict = {}
-
-#Message Stack
-global MESSAGE_STACK
-MESSAGE_STACK = []
-
-#Selected group title
-global SELECTED_GROUP_TITLE
-SELECTED_GROUP_TITLE = 'Whale Group'
-
-#trigger for bot loop. set after selectGroups runs successfully
-global READY
-READY = False
-#________________________________________________________________________#
 #Lets the user select groups to track
-def selectGroups():
+def selectGroups(groupChats):
     index = 1
-    global groupChats
     for chat in groupChats:
         title= chat.find_elements_by_css_selector("div.im_dialog_peer")[0].text
         print(str(index) + '. ' + title)
@@ -65,23 +32,19 @@ def selectGroups():
     for index in listOfIndexOfChats:
         userChats.append(groupChats[index-1])
 
-    #set global to the selectedUserChats
-    groupChats = userChats
+    #return userChats
+    return userChats
     
     print('selectGroups ran successfully')
-    print('new chats: \n')
 
 #________________________________________________________________________#
 #Creates dictionairy of last messages in each group
-def initializeDict():
-    print(lastMessageDict)
-#     for some reason error on first run after log in unless this line is her
-#    groupChats[0].click
-    
+def initializeDict(groupChats):
+    lastMessageDict = {}
     for chat in groupChats:
         chat.click()
 #         time.sleep(2)
-        SELECTED_GROUP_TITLE = chat.find_elements_by_css_selector("div.im_dialog_peer")[0].text
+        selectedGroupTitle = chat.find_elements_by_css_selector("div.im_dialog_peer")[0].text
         while True:
             try:
                 messages = browser.find_elements_by_class_name("im_message_text")
@@ -91,17 +54,17 @@ def initializeDict():
                     textMessages.append(webElement.text)
                 #for some reason there were empty strings being grabbed by selenium. removing them here    
                 textMessages = list(filter(None, textMessages))
-                key_gc = SELECTED_GROUP_TITLE
+                key_gc = selectedGroupTitle
                 lastMessageDict[key_gc] = textMessages[-1]
-                print('finished initializing: ' + SELECTED_GROUP_TITLE)
+                print('finished initializing: ' + selectedGroupTitle)
                 break
             except:
-                print(SELECTED_GROUP_TITLE + 'did not load yet, trying again.')
+                print(selectedGroupTitle + 'did not load yet, trying again.')
     print(lastMessageDict)
     print('initializeDict ran successfully')
 #________________________________________________________________________#
 #groupQueueBuilder
-def groupQueueBuilder():
+def groupQueueBuilder(groupChats):
     for groupChat in groupChats:
         oneBadgeArray = groupChat.find_elements_by_css_selector("span.im_dialog_badge.badge")
 #         TO DO : make it not take messages from HOME GROUP
@@ -110,28 +73,25 @@ def groupQueueBuilder():
             oneTitleArray = groupChat.find_elements_by_css_selector("div.im_dialog_peer")
             if(oneTitleArray[0].text not in groupQueue):
                 groupQueue.append(oneTitleArray[0].text)
+    return groupQueue
     print('groupQueueBuilder ran successfully')
-    print(groupQueue)
 #________________________________________________________________________#
-#groupFinder - Takes in name of group finds it selects it
+#groupFinder - Takes in name of group finds it selects it, returns title of slected chat
 def groupFinder(name):
-    groupQueue.popleft()
-    #print(chats)
     for chat in groupChats:
         titleArray = chat.find_elements_by_css_selector("div.im_dialog_peer")
         if(titleArray[0].text == name):
             chat.click()
-            global SELECTED_GROUP_TITLE
-            SELECTED_GROUP_TITLE = titleArray[0].text
-            break;
-            
-    print('groupFinder ran successfully')
-print(groupQueue)
+            print('groupFinder ran successfully')
+            return(titleArray[0].text)
+    print('groupFinder Could not find group: '+ name)
+    return 'noGroupError'
+    
 #________________________________________________________________________#
 #Create push method to click on 'home chat' and paste and send it there.
 # checks via dictionary which messages to push
-def createMessageStack():
-    MESSAGE_STACK.clear()
+def createMessageStack(selectedGroupTitle, lastMessageDict):
+    messageStack = []
     messages = browser.find_elements_by_class_name("im_message_text")
     textMessages = []
     #change array of WebElements to array of strings. easier to work with, avoid errors
@@ -141,25 +101,23 @@ def createMessageStack():
     textMessages = list(filter(None, textMessages))
     #reversing list to make FIFO
     textMessages.reverse()
-    print(lastMessageDict[SELECTED_GROUP_TITLE])
+    print(lastMessageDict[selectedGroupTitle])
     for message in textMessages:
         
-        if(message != lastMessageDict[SELECTED_GROUP_TITLE]):
-            MESSAGE_STACK.append(message)
+        if(message != lastMessageDict[selectedGroupTitle]):
+            messageStack.append(message)
         else:
 #             SHOULD RUN WHEN MESSAGES ARE PUSHED
-            
             break
+    return messageStack
     print('createMessageStack ran successfully')
-    print('message Stack:')
-    print(MESSAGE_STACK)
 #________________________________________________________________________#
 # Broadcast Method
-def broadCast():
+def broadCast(messageStack, selectedGroupTitle, groupChats):
     #select home chat
     print('Will now be sending:')
-    print(MESSAGE_STACK)
-    MESSAGE_STACK.reverse()
+    print(messageStack)
+    messageStack.reverse()
     for chat in groupChats:
         titleArray = chat.find_elements_by_css_selector("div.im_dialog_peer")
         if(titleArray[0].text == HOME_GROUP):
@@ -168,17 +126,19 @@ def broadCast():
     
     #once homechat is selected, send messages one by one.
     textBox = browser.find_elements_by_class_name('composer_rich_textarea')[0]
-    for message in MESSAGE_STACK:        
-        textBox.send_keys(SELECTED_GROUP_TITLE + ": " + message)
+    for message in messageStack:        
+        textBox.send_keys(selectedGroupTitle + ": " + message)
         textBox.send_keys(Keys.RETURN)
-    if(len(MESSAGE_STACK) != 0):
-        lastMessageDict[SELECTED_GROUP_TITLE] = MESSAGE_STACK[-1]
+    if(len(messageStack) != 0):
+        lastMessageDict[selectedGroupTitle] = messageStack[-1]
     #clean up after sending messages.
-    MESSAGE_STACK.clear()
+    messageStack.clear()
     print('broadcast ran successfully')
-print(SELECTED_GROUP_TITLE)
 #________________________________________________________________________#
 #User prompt to run BotLoop
+global READY
+READY = False
+
 while True:
     print('inside user prompt')
     try:
@@ -190,7 +150,6 @@ while True:
 
     else:
         if(userPrompt == 'ready'):
-            global READY
             READY = True
             break
         else:
@@ -199,30 +158,32 @@ while True:
 #________________________________________________________________________#
 #BotLoop
 #grabs all groupchats
-groupChats = browser.find_elements_by_class_name("im_dialog_wrap")
 
-while True:
-    print('inside botLoop')
-    try:
-        if (READY):
-            selectGroups()
-            initializeDict()
-            timeout = 30
-            timeout_start = time.time()
-            while time.time() < timeout_start + timeout:
-                if(len(list(groupQueue)) == 0):
-                    #timeout for x second(s)
-                    time.sleep(3)
-                    groupQueueBuilder()
-                else:
-                    groupFinder(groupQueue[0])
-                    createMessageStack()
-                    broadCast()
-                    
-            print('Finished BotLoop')
-                
-                
-                    #get first in queue and cache / print all messages from it.
-                    #For our purpose, batch work scheduler would be better. **prioritizes active groupchats**
-    except:
-        pass
+# while True:
+#     print('inside botLoop')
+# try:
+if (READY):
+    #Create Runtime Variables inside here.
+    groupChats = browser.find_elements_by_class_name("im_dialog_wrap") 
+    groupQueue = deque([])
+    #_____________________________________
+    trackedChats = selectGroups(groupChats)
+    lastMessageDict = initializeDict(trackedChats)
+    timeout = 90
+    timeout_start = time.time()
+    while time.time() < timeout_start + timeout:
+        if(len(list(groupQueue)) == 0):
+            time.sleep(3)
+            groupQueue = groupQueueBuilder(trackedChats)
+        else:
+            selectedGroupTitle = groupFinder(groupQueue.popleft())
+            messageStack = createMessageStack(selectedGroupTitle, lastMessageDict)
+            broadCast(messageStack, selectedGroupTitle, userChats)
+            
+    print('Finished BotLoop')
+        
+        
+            #get first in queue and cache / print all messages from it.
+            #For our purpose, batch work scheduler would be better. **prioritizes active groupchats**
+    # except:
+        # pass
